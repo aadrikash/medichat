aa75df383b3c9470896f86333dac599bdc50a950
aadrika sharma <aadrikasharma10022004@gmail.com>
2026-01-26 02:41:54 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medichat",
+    version="0.1.0",
+    author="Aadrika sharma",
+    author_email="2023pceadaadrika001@poornima.org",
+    packages=find_packages(),
+    install_requires=[]
+)
